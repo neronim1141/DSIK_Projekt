7b3899d3c98d803c53c4adc5f5881a4d38a5d95d
Kacper Kruczek <neronim66@gmail.com>
2018-05-08 17:21:19 +0200

    player input

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-const dgram = require('dgram');
-const Game = require('./game');
-const Commands = require('./commands');
+const dgram = require("dgram");
+const Game = require("./game");
+const Commands = require("./commands");
 //initialize readline
-const rl = require('readline').createInterface({
+const rl = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout
 });
-const client = dgram.createSocket('udp4');
-
+const client = dgram.createSocket("udp4");
+var adresIP, port;
 let game,
   AI = false,
   PlayerName,
@@ -17,20 +17,36 @@ let game,
 
 const main = async () => {
   let answer;
+  answer = await question("Enter ip address");
+  adresIP = answer;
+  answer = await question("Enter port");
+  port = answer;
   //wait for answer if Player is AI
-  answer = await question('AI player y/n?');
-  if (answer === 'y') {
-    AI = true;
-  }
+
+  do {
+    answer = await question("AI player y/n?");
+    if (!answer && answer != 'y' && answer != 'n') {
+      console.log("y/n:");
+
+    } else {
+      if (answer === "y") {
+        AI = true;
+      }
+    }
+  } while (!answer && answer != 'y' && answer != 'n');
   //wait for Player name
-  answer = await question('playerName:');
+
+  do {
+    answer = await question("playerName:");
+
+  } while (!answer);
   PlayerName = answer;
 
   // send connect message
   client.send(
     Buffer.from(`${Commands.CONNECT} ${PlayerName}`),
-    41234,
-    'localhost',
+    port,
+    adresIP.toString(),
     err => {}
   );
 };
@@ -39,8 +55,8 @@ const main = async () => {
 main();
 
 //receive commands from server
-client.on('message', (msg, rinfo) => {
-  let command = msg.toString().split(' ');
+client.on("message", (msg, rinfo) => {
+  let command = msg.toString().split(" ");
 
   switch (command[0]) {
     //game started
@@ -53,13 +69,14 @@ client.on('message', (msg, rinfo) => {
       }
       //add players to game
       game.addPlayers(players);
+      displayBoard();
       //show board
       break;
 
     case Commands.YOURTURN:
       makeMove();
       break;
-    // if anyone (and you) made move
+      // if anyone (and you) made move
     case Commands.MOVE:
       turnResolve(command[1], command[2], command[3]);
       break;
@@ -112,18 +129,21 @@ async function makeMove() {
 
     do {
       //wait for player move
-      answer = await question('yourmove x y?');
-      let move = answer.split(' ');
+      answer = await question("yourmove x y?");
+
+      let move = answer.split(" ");
       x = parseInt(move[0]);
       y = parseInt(move[1]);
+      if (!game.canMove(PlayerName, x, y))
+        console.log("Can't move");
       // while x and y are no good
     } while (!game.canMove(PlayerName, x, y));
   }
   //send move message
   client.send(
     Buffer.from(`${Commands.MOVE} ${PlayerName} ${x} ${y}`),
-    41234,
-    'localhost',
+    port,
+    adresIP.toString(),
     err => {}
   );
 }
@@ -132,8 +152,8 @@ function turnResolve(name, x, y) {
   // add move to board
   game.add(name, parseInt(x), parseInt(y));
   //show board
-  if (PlayerName != name) console.log('got Move:', name, x, y);
-  else console.log('your Move:', name, x, y);
+  if (PlayerName != name) console.log("got Move:", name, x, y);
+  else console.log("your Move:", name, x, y);
   displayBoard();
 }
 
@@ -144,24 +164,25 @@ function question(question) {
     });
   });
 }
+
 function displayBoard() {
-  let yrows = '\x1b[47m\x1b[30m  ';
-  for (let i = 0; i < game.map_.length; i++) yrows += i + ' ';
-  yrows += '\x1b[0m';
+  let yrows = "\x1b[47m\x1b[30m  ";
+  for (let i = 0; i < game.map_.length; i++) yrows += i + " ";
+  yrows += "\x1b[0m";
   console.log(yrows);
 
   for (let y = 0; y < game.map_.length; y++) {
-    let row = '';
+    let row = "";
     for (let x = 0; x < game.map_.length; x++) {
-      if (x == 0) row += '\x1b[47m\x1b[30m' + y + ' \x1b[0m';
+      if (x == 0) row += "\x1b[47m\x1b[30m" + y + " \x1b[0m";
       if (game.map_[y][x].playerName == null) {
-        row += '\x1b[37m' + game.map_[y][x].points;
+        row += "\x1b[37m" + game.map_[y][x].points;
       } else if (game.map_[y][x].playerName == PlayerName) {
-        row += '\x1b[32m' + game.map_[y][x].points;
+        row += "\x1b[32m" + game.map_[y][x].points;
       } else if (game.map_[y][x].playerName != null) {
-        row += '\x1b[31m' + game.map_[y][x].points;
+        row += "\x1b[31m" + game.map_[y][x].points;
       }
-      row += ' \x1b[0m';
+      row += " \x1b[0m";
     }
     console.log(row);
   }
