62b55321bda9e90d452f72b3898b38ee86dea633
Kacper Kruczek <neronim66@gmail.com>
2018-05-08 11:35:17 +0200

    server questions

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-const Commands = require('./commands');
-const Player = require('./player');
-const Game = require('./game');
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+const Commands = require("./commands");
+const Player = require("./player");
+const Game = require("./game");
 let maxPlayers = 4,
   gameSize = 8,
   players = [],
   turn = 0,
   game;
-
-server.on('listening', () => {
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const main = async () => {
+  let answer;
+  //wait for answer if Player is AI
+  do {
+    answer = await question("max players?(2-4)?");
+    if (!answer || parseInt(answer) < 2 || parseInt(answer) > 4) {
+      console.log("Wrong max players type secodn time:");
+      continue;
+    }
+  } while (!answer || parseInt(answer) < 2 || parseInt(answer) > 4);
+  //wait for Player name
+  maxPlayers = parseInt(answer);
+  do {
+    answer = await question("board size(4-8:)");
+    if (!answer || parseInt(answer) < 4 || parseInt(answer) > 8) {
+      console.log("Wrong board size type secodn time:");
+      continue;
+    }
+  } while (!answer || parseInt(answer) < 4 || parseInt(answer) > 8);
+  gameSize = parseInt(answer);
+  server.bind(41234);
+};
+main();
+server.on("listening", () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.on('error', err => {
+server.on("error", err => {
   console.log(`server error:\n${err.stack}`);
   //send  to players if on server is any error
   for (let player of players) {
@@ -28,12 +54,12 @@ server.on('error', err => {
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
+server.on("message", (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-  let command = msg.toString().split(' ');
+  let command = msg.toString().split(" ");
   switch (command[0]) {
     //connect player
-    case 'connect':
+    case "connect":
       connectPlayer(rinfo, command[1]);
       break;
     //player made move
@@ -43,7 +69,6 @@ server.on('message', (msg, rinfo) => {
   }
 });
 
-server.bind(41234);
 // server listening 0.0.0.0:41234
 
 function connectPlayer(rinfo, name) {
@@ -61,9 +86,9 @@ function connectPlayer(rinfo, name) {
     // if max players are in the game
     if (players.length === maxPlayers) {
       // create string with list of player name
-      let list = '';
+      let list = "";
       for (let player of players) {
-        list += ' ' + player.name;
+        list += " " + player.name;
       }
 
       for (let player of players) {
@@ -129,12 +154,14 @@ function sendTurn() {
   let isOnBoard = game.playersOnBoard().indexOf(players[turn].name) >= 0;
   let playersOnBoard = game.playersOnBoard().length;
   if (playersOnBoard == 1 && isOnBoard) {
+    console.log("Player: " + players[turn].name + " WINS!");
     server.send(
       Buffer.from(Commands.WIN),
       players[turn].port,
       players[turn].address,
       err => {
         server.close();
+        process.exit();
       }
     );
     removePlayer(turn);
@@ -157,41 +184,48 @@ function sendTurn() {
 }
 
 function displayBoard() {
-  let yrows = '\x1b[47m\x1b[30m  ';
-  for (let i = 0; i < game.map_.length; i++) yrows += i + ' ';
-  yrows += '\x1b[0m';
+  let yrows = "\x1b[47m\x1b[30m  ";
+  for (let i = 0; i < game.map_.length; i++) yrows += i + " ";
+  yrows += "\x1b[0m";
   console.log(yrows);
 
   for (let y = 0; y < game.map_.length; y++) {
-    let row = '';
+    let row = "";
     for (let x = 0; x < game.map_.length; x++) {
-      if (x == 0) row += '\x1b[47m\x1b[30m' + y + ' \x1b[0m';
+      if (x == 0) row += "\x1b[47m\x1b[30m" + y + " \x1b[0m";
       if (game.map_[y][x].playerName == null) {
-        row += '\x1b[37m' + game.map_[y][x].points;
+        row += "\x1b[37m" + game.map_[y][x].points;
       } else if (
         players[0] != undefined &&
         game.map_[y][x].playerName == players[0].name
       ) {
-        row += '\x1b[32m' + game.map_[y][x].points;
+        row += "\x1b[32m" + game.map_[y][x].points;
       } else if (
         players[1] != undefined &&
         game.map_[y][x].playerName == players[1].name
       ) {
-        row += '\x1b[33m' + game.map_[y][x].points;
+        row += "\x1b[33m" + game.map_[y][x].points;
       } else if (
         players[2] != undefined &&
         game.map_[y][x].playerName == players[2].name
       ) {
-        row += '\x1b[34m' + game.map_[y][x].points;
+        row += "\x1b[34m" + game.map_[y][x].points;
       } else if (
         players[3] != undefined &&
         game.map_[y][x].playerName == players[3].name
       ) {
-        row += '\x1b[35m' + game.map_[y][x].points;
+        row += "\x1b[35m" + game.map_[y][x].points;
       }
-      row += ' \x1b[0m';
+      row += " \x1b[0m";
     }
     console.log(row);
   }
   console.log();
+}
+function question(question) {
+  return new Promise((resolve, reject) => {
+    rl.question(question, answer => {
+      resolve(answer);
+    });
+  });
 }
